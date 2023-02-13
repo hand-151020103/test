b76d0145d626d7a9f6fbf0d8c31eb1c308a26c43
Dashan <wdshhit@163.com>
2023-02-13 17:41:53 +0800

    commit02

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -4,5 +4,6 @@ import org.w3c.dom.ls.LSOutput;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello, Git1");
+        System.out.println("Hello,Git2");
     }
 }
